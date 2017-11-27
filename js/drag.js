b663a34f6757fe59ccbcbf8d9e8ffeb94b5a666b
李学韬 <lxt@lixuetaodeMacBook-Pro.local>
2017-11-27 10:23:31 +0800

    resoft

---
+++ js/drag.js
@@ -0,0 +1,39 @@
+/**
+ * Created by tanghuan on 2017/11/26.
+ */
+(function($) {
+    $.fn.move = function() {
+        return $(this).each(function() {
+            var oThis = $(this),
+                _isMove = false,
+                _x = 0,
+                _y = 0,
+                _width = $(this).width(),
+                _height = $(this).height(),
+                _winWidth = oThis.parent().width(),
+                _winHeight = oThis.parent().height();
+            $(this).mousedown(function(e) {
+                console.log(_winHeight);
+                _isMove = true;
+                console.log(e.clientX)
+                _x = e.clientX - parseInt($(this).css("left"));
+                _y = e.clientY - parseInt($(this).css("top"));
+//                    _x= e.offsetX;
+//                    _y= e.offsetY;
+//                    console.log(e.clientX,e.clientY,_x,_y,_x1,_y1);
+            });
+            $(document).mousemove(function(e) {
+                if( _isMove ) {
+                    window.e ? window.e.returnValue = false : e.preventDefault();
+                    var x = e.clientX - _x ;
+                    var y = e.clientY - _y ;
+                    //x = Math.max(0, Math.min(_winWidth - _width, x));
+                    //y = Math.max(0, Math.min(_winHeight - _height, y));
+                    oThis.css({"left":parseInt(x), "top":parseInt(y)});
+                }
+            }).mouseup(function() {
+                _isMove = false;
+            });
+        });
+    }
+})(jQuery);
